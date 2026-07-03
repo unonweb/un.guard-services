f69886c0b9e65572110b3d4fd20140b98a0a2f53
unonweb <unonweb@posteo.de>
2026-07-03 10:57:38 +0200

    - add checks - add early return - cleanup logic

--- src/lib/alert.sh
+++ src/lib/alert.sh
@@ -3,7 +3,6 @@
 # - MAIL_ALERT
 # - MAIL_TO
 # - MAIL_SUBJECT
-# - MAIL_TO
 
 function alert {
 
@@ -13,21 +12,40 @@ function alert {
 		return 0
 	fi
 
-	if (( MAIL_ALERT )) && [[ -z "${MAIL_TO}" ]]; then
-		log "<3> Required var not set: MAIL_TO"
-		return 1
+	if [[ -z "${alert_msg}" ]]; then
+		return 0
 	fi
 
-	if [[ -n "${alert_msg}" ]]; then
+	# CHECK vars
+	for var in MAIL_TO; do
+		if [[ -z "${!var}" ]]; then
+			log "<3> Required var missing: ${var}"
+			return 1
+		fi
+	done
 
-		# ALERT
-		alert_msg_header+="DATE: $(date "+%Y-%m-%d %H:%M:%S")\n"
-		alert_msg_header+="HOSTNAME: ${HOSTNAME}\n"
-		alert_msg_header+="---\n\n"
-		alert_msg_header+="NEW SERVICES:\n"
-		
-		echo -e "${alert_msg_header}${alert_msg}" | \
-		mail -s "${MAIL_SUBJECT}" "${MAIL_TO}" 2>/dev/null
+	# CHECK internal deps
+	for fctn in log; do
+    	if ! declare -f "${fctn}" > /dev/null; then
+        	echo "<3> Error: Required function missing: ${fctn}" >&2
+        	return 1
+    	fi
+	done
+
+	# CHECK external deps
+	for cmd in mail; do
+    	if ! command -v "${cmd}" &> /dev/null; then
+        	log "<3> Error: Required external cmd missing: ${cmd}" >&2
+        	return 1
+    	fi
+	done
+
+	# ALERT
+	alert_msg_header+="DATE:		$(date "+%Y-%m-%d %H:%M:%S")\n"
+	alert_msg_header+="HOSTNAME:	${HOSTNAME}\n"
+	alert_msg_header+="---\n\n"
+	alert_msg_header+="NEW SERVICES:\n"
 	
-	fi
+	echo -e "${alert_msg_header}${alert_msg}" | \
+	mail -s "${MAIL_SUBJECT}" "${MAIL_TO}" 2>/dev/null
 }
